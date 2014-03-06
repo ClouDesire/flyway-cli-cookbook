d0cd2851f950760f90b67a670ce246f81587c5d8
Giovanni Toraldo <g.toraldo@liberologico.com>
2014-03-06 18:34:06 +0100

    Adjusted flyway-cli default version to 2.3
    Please note that they removed -dist from url, so you can get a 404.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default[:flyway][:version] = "2.1.1"
+default[:flyway][:version] = "2.3"
 default[:flyway][:installation_path] = "/opt/flyway"
 default[:flyway][:jdbc_url] = "jdbc:postgresql://localhost:5432/database"
 default[:flyway][:jdbc_username] = ""
@@ -6,9 +6,9 @@ default[:flyway][:jdbc_password] = ""
 default[:flyway][:jdbc_driver][:postgresql][:version] = "9.3-1100-jdbc4"
 default[:flyway][:jdbc_driver][:mysql][:version] = "5.1.28"
 default[:flyway][:include_java_recipe] = true
- 
+
 
 default[:flyway][:version_installed] = "0"
-default[:flyway][:base_url] = "http://repo1.maven.org/maven2/com/googlecode/flyway/flyway-commandline/VERSION/flyway-commandline-VERSION-dist.tar.gz"
+default[:flyway][:base_url] = "http://repo1.maven.org/maven2/com/googlecode/flyway/flyway-commandline/VERSION/flyway-commandline-VERSION.tar.gz"
 default[:flyway][:jdbc_driver][:postgresql][:url] = "http://search.maven.org/remotecontent?filepath=org/postgresql/postgresql/VERSION/postgresql-VERSION.jar"
 default[:flyway][:jdbc_driver][:mysql][:url] = "http://search.maven.org/remotecontent?filepath=mysql/mysql-connector-java/VERSION/mysql-connector-java-VERSION.jar"
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: flyway-cli
 # Recipe:: default
 #
-# Copyright (C) 2014 YOUR_NAME
+# Copyright (C) 2014 ClouDesire
 #
 # All rights reserved - Do Not Redistribute
 #
