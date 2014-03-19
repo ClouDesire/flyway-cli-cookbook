27dc0a407bbebd343cc49f069cca9e6e66895e90
Giovanni Toraldo <g.toraldo@liberologico.com>
2014-03-19 21:15:28 +0100

    Enabling initOnMigrate and enable management via attribute
    Release v0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dev@cloudesire.com'
 license          'Apache 2.0'
 description      'Installs and execute flyway cli'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 supports         'ubuntu'
 depends          'apt'
 depends          'java'
--- recipes/migrate.rb
+++ recipes/migrate.rb
@@ -1,3 +1,3 @@
 execute "do migrations" do
-  command node[:flyway][:installation_path] + "/flyway migrate"
+  command node[:flyway][:installation_path] + "/flyway migrate -initOnMigrate=" + node[:flyway][:init_on_migrate]
 end
