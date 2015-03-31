13633942a86e8a33574aa9b783738a376b3e3259
Manuel Mazzuola <manuel.mazzuola@cloudesire.com>
2015-03-31 09:53:10 +0200

    On windows run flyway and set java home using the windows_batch provider

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dev@cloudesire.com'
 license          'Apache 2.0'
 description      'Installs and execute flyway cli'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.2'
+version          '0.2.3'
 supports         'ubuntu'
 depends          'apt'
 depends          'java', '~> 1.17'
--- recipes/migrate.rb
+++ recipes/migrate.rb
@@ -7,14 +7,6 @@ installation_path = node[:flyway][:installation_path]
 # Flyway don't care if platform is windows or linux
 migrations_path = node[:flyway][:migrations_path].gsub(/\\/, '/')
 
-if platform_family?("windows")
-  windows_batch 'set_java_home' do
-    code <<-EOH
-    set JAVA_HOME=#{node[:java][:java_home]}
-    EOH
-  end
-end
-
 node[:flyway][:confs].each do |key, confs|
   file installation_path + "/conf/#{key}.properties" do
     action :create
@@ -23,10 +15,22 @@ flyway.url=#{confs[:jdbc_url]}
 flyway.user=#{confs[:jdbc_username]}
 flyway.password=#{confs[:jdbc_password]}
 flyway.locations=filesystem:#{migrations_path}
+java.home=#{node[:java][:java_home]}
     EOH
   end
 
-  execute "do migrations" do
-    command installation_path + "/flyway migrate -configFile=#{installation_path}/conf/#{key}.properties -initOnMigrate=" + node[:flyway][:init_on_migrate]
+  flyway_command = installation_path + "/flyway migrate -configFile=#{installation_path}/conf/#{key}.properties -initOnMigrate=" + node[:flyway][:init_on_migrate]
+
+  if platform?("windows")
+    windows_batch 'set_java_home_and_run_flyway' do
+      code <<-EOH
+      set JAVA_HOME=#{node[:java][:java_home]}
+      #{flyway_command}
+      EOH
+    end
+  else
+    execute "do migrations" do
+      command flyway_command
+    end
   end
 end
