e5bf19e50e98d316c45e7751f42d7e36b3b01cd2
Giovanni Toraldo <me@gionn.net>
2015-05-05 23:17:05 +0200

    Release 0.3.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dev@cloudesire.com'
 license          'Apache 2.0'
 description      'Installs and execute flyway cli'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.3.1'
 supports         'ubuntu'
 supports         'windows'
 
