98af3e719e2c050157e08e51326b9c9617eddb69
Giovanni Toraldo <me@gionn.net>
2015-05-14 23:21:29 +0200

    Minor fix: added issues_url to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,11 @@
 name             'flyway-cli'
 maintainer       'ClouDesire'
 maintainer_email 'dev@cloudesire.com'
-license          'Apache 2.0'
+license          'Apache v2.0'
 description      'Installs and execute flyway cli'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/ClouDesire/flyway-cli-cookbook'
+issues_url       'https://github.com/ClouDesire/flyway-cli-cookbook/issues'
 version          '0.3.1'
 supports         'ubuntu'
 supports         'windows'
