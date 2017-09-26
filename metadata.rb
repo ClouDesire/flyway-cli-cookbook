74c3936324344b75355d735e832615d02c0a5da7
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2017-09-26 15:55:30 +0200

    Release 1.0.1

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs and execute flyway cli'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/ClouDesire/flyway-cli-cookbook' if respond_to?(:source_url)
 issues_url       'https://github.com/ClouDesire/flyway-cli-cookbook/issues' if respond_to?(:issues_url)
-version          '1.0.0'
+version          '1.0.1'
 supports         'ubuntu'
 supports         'windows'
 
