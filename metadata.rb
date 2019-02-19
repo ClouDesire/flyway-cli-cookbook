981e5899fb88544d7aa2cdacba0d99c6a6d3a3cd
Kyle McGovern <kyle.mcgovern@cerner.com>
2019-02-18 18:16:28 -0600

    update java dependency to 2.x

--- metadata.rb
+++ metadata.rb
@@ -6,10 +6,10 @@ description      'Installs and execute flyway cli'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/ClouDesire/flyway-cli-cookbook' if respond_to?(:source_url)
 issues_url       'https://github.com/ClouDesire/flyway-cli-cookbook/issues' if respond_to?(:issues_url)
-version          '1.0.1'
+version          '1.1.0'
 supports         'ubuntu'
 supports         'windows'
 
-depends          'java', '~> 1.17'
+depends          'java', '~> 2.0'
 depends          'ark'
 depends          'compat_resource' # For custom resource compilation on chef-client versions < 12.5
